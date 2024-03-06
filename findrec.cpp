7d5320536699d7a56dbc8408605364875bfa7602
Marcos Costa <marcoscosta@Marcoss-MacBook-Pro.local>
2024-03-05 22:42:06 -0400

    findrec done

---
+++ findrec.cpp
@@ -0,0 +1,20 @@
+#include <iostream>     
+#include "hash.hpp"
+
+int main(int argc, char *argv[])
+{
+    if(argc<2) {
+        cout << "Digite o ID que deseja buscar.\n\nEx: findrec <ID>\n";
+        return 1;
+    }
+
+    fstream *hashFile = new fstream(DATA_FILE,fstream::in|ios::binary);
+
+    if (hashFile->is_open()) {
+        buscaRegistroPorId(hashFile,atoi(argv[1]));   
+    }
+    else {
+        cout << "Erro ao abrir o arquivo.\n";
+    }
+    return 0;
+}
